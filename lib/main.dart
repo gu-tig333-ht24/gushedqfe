a1522652c57b29f1292792950c2456e3823bf7d5
hedqvistpersson <124053990+hedqvistpersson@users.noreply.github.com>
2024-09-12 15:08:20 +0200

    update 2

--- lib/main.dart
+++ lib/main.dart
@@ -19,60 +19,89 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Todo {
+  final String syssla;
+  final String status;
+  final String klar = 'är du verkligen klar med detta?';
+
+  Todo(this.syssla, this.status);
+}
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Todo> todos = [
+      Todo('laga mat', 'ej klar'),
+      Todo('laga mattan', 'klar'),
+      Todo('tvätta', 'ej klar'),
+      Todo('plugga', 'klar'),
+    ];
     return Scaffold(
       appBar: AppBar(
         title: const Text('Att göra lista'),
         centerTitle: true,
         backgroundColor: Colors.orange,
       ),
-      body: Column(
-        children: [
-          _item('städa', 'ej klar'),
-          _item('tvätta', 'klar'),
-          _item('plugga', 'om tid finns'),
-        ],
+      body: ListView(
+        children: todos.map((todo) => _item(context, todo)).toList(),
       ),
     );
   }
 
-  Widget _item(String syssla, String status) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Padding(
-          padding: EdgeInsets.all(10),
-          child: Container(
-            width: 40,
-            height: 40,
-            decoration:
-                BoxDecoration(shape: BoxShape.rectangle, color: Colors.blue),
+  Widget _item(BuildContext context, Todo todo) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Time(todo)));
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration:
+                  BoxDecoration(shape: BoxShape.rectangle, color: Colors.blue),
+            ),
           ),
-        ),
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                syssla,
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(status)
-            ],
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  todo.syssla,
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text(todo.status)
+              ],
+            ),
           ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(right: 15),
-          child: Icon(Icons.chevron_right),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.only(right: 15),
+            child: Icon(Icons.chevron_right),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Time extends StatelessWidget {
+  final Todo todo;
+  Time(this.todo);
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      appBar: AppBar(title: Text(todo.syssla)),
+      body: Text(todo.klar),
     );
   }
 }
